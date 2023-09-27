e2f809441e01b041979e469c27fc2ff851d61403
Nithin Joji0756 <nithinjoji0756@gmail.com>
2023-09-28 01:28:23 +0530

    search is also implemented in admin product management and admin usermanagement use vanila javascript

---
+++ public/js/search.js
@@ -0,0 +1,19 @@
+
+const searchInput = document.getElementById('userSearch');
+const userRows = document.querySelectorAll('tbody tr');
+
+
+searchInput.addEventListener('input', ()=>{
+  const searchText = searchInput.value.toLowerCase();
+
+
+  userRows.forEach(userRow => {
+    const userName = userRow.querySelector('.userName').textContent.toLowerCase();
+
+    if (userName.includes(searchText)) {
+      userRow.style.display = ''; 
+    } else {
+      userRow.style.display = 'none'; 
+    }
+  });
+});
