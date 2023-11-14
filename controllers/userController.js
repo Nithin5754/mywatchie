d3fe5a18dd2aa1fe0f07fa9b137c1a8cd43e38d5
Nithin Joji0756 <nithinjoji0756@gmail.com>
2023-11-14 22:43:44 +0530

    change the usercart in usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -122,18 +122,18 @@ const homepage = async (req, res) => {
     isLogin = true;
     iswallet='/userwallet'
 
-    const cartItems = await Cart.findOne({ userId: verifyUserEmail._id });
-    if (!cartItems) {
-      console.log('no cart');
-      let userCart = new Cart({
-        userId: verifyUserEmail._id,
-        items: [],
-        total: null,
-        totalQuantity: null,
-      });
+    // const cartItems = await Cart.findOne({ userId: verifyUserEmail._id });
+    // if (!cartItems) {
+    //   console.log('no cart');
+    //   let userCart = new Cart({
+    //     userId: verifyUserEmail._id,
+    //     items: [],
+    //     total: null,
+    //     totalQuantity: null,
+    //   });
 
-      await userCart.save();
-    }
+    //   await userCart.save();
+    // }
 
 
 
