50b598303ceddeb69cf21610c9feb9c303ddeb71
Nithin Joji0756 <nithinjoji0756@gmail.com>
2023-11-14 15:51:52 +0530

    search bar placed in a button

--- public/js/productsearch.js
+++ public/js/productsearch.js
@@ -7,3 +7,16 @@ items.forEach(item => {
     window.location.href = `/productList/${categoryId}?searchContent=${searchContent}`;
   });
 });
+
+
+
+
+
+const itemsAll = document.querySelectorAll('.searchable-item-all');
+
+itemsAll.forEach(item => {
+  item.addEventListener('input', event => {
+    const searchContent = event.target.value;
+    window.location.href = `/productFullList?searchContent=${searchContent}`;
+  });
+});
